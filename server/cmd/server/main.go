1dd43e32acdbd7a37d6b87d577e5031f5f05dfec
Nitish Sinha <nitishgoku01@gmail.com>
2024-07-21 10:52:38 +0530

    renaming to localhost

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -14,7 +14,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "127.0.0.1:8000",
+		Addr:         "localhost:8000",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
